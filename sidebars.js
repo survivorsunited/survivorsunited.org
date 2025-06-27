1bbda67c26ecf7a71e6bd3e4ad9cb162015d39a3
wildone <max.barrass@gmail.com>
2025-06-28 04:43:04 +1000

    flatten sidebar.

--- sidebars.js
+++ sidebars.js
@@ -8,63 +8,57 @@ const sidebars = {
     "terminology",
     {
       type: "category",
-      label: "Minecraft",
+      label: "Getting Started",
       items: [
-        {
-          type: "category",
-          label: "Getting Started",
-          items: [
-            "minecraft/getting-started"
-          ]
-        },
-        {
-          type: "category",
-          label: "Installation",
-          items: [
-            "minecraft/installation/java",
-            "minecraft/installation/minecraft",
-            "minecraft/installation/fabric"
-          ]
-        },
-        {
-          type: "category",
-          label: "Mods",
-          items: [
-            "minecraft/mods/installation",
-            "minecraft/mods/folder-setup"
-          ]
-        },
-        {
-          type: "category",
-          label: "Server",
-          items: [
-            "minecraft/server/connection",
-            "minecraft/server/discord"
-          ]
-        },
-        {
-          type: "category",
-          label: "First Steps",
-          items: [
-            "minecraft/first-steps/things-to-do-first",
-            "minecraft/first-steps/things-to-visit"
-          ]
-        },
-        {
-          type: "category",
-          label: "Configuration",
-          items: [
-            "minecraft/configuration/keybinds",
-            "minecraft/configuration/shaders"
-          ]
-        },
-        {
-          type: "category",
-          label: "Troubleshooting",
-          items: [
-            "minecraft/troubleshooting/errors"
-          ]
-        }
+        "minecraft/getting-started"
+      ]
+    },
+    {
+      type: "category",
+      label: "Installation",
+      items: [
+        "minecraft/installation/java",
+        "minecraft/installation/minecraft",
+        "minecraft/installation/fabric"
+      ]
+    },
+    {
+      type: "category",
+      label: "Mods",
+      items: [
+        "minecraft/mods/installation",
+        "minecraft/mods/folder-setup"
+      ]
+    },
+    {
+      type: "category",
+      label: "Server",
+      items: [
+        "minecraft/server/connection",
+        "minecraft/server/discord"
+      ]
+    },
+    {
+      type: "category",
+      label: "First Steps",
+      items: [
+        "minecraft/first-steps/things-to-do-first",
+        "minecraft/first-steps/things-to-visit"
+      ]
+    },
+    {
+      type: "category",
+      label: "Configuration",
+      items: [
+        "minecraft/configuration/keybinds",
+        "minecraft/configuration/shaders"
+      ]
+    },
+    {
+      type: "category",
+      label: "Troubleshooting",
+      items: [
+        "minecraft/troubleshooting/errors"
       ]
     },
     {
